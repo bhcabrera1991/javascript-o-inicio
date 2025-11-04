c004439cd3ccafb87188e1eb2cb503deba737e2f
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-03 22:26:04 -0300

    Aula 1(03112025)
    Boas práticas
    Case sensitive
    Sintaxe básica e comentário
    Declaração de variáveis
    Console e Debug
    Tipos de dados

---
+++ aula0001/app.js
@@ -0,0 +1,71 @@
+// Boas práticas
+/*1. Escreva código legível
+2Use const e let em vez de var
+Evite código duplicado*/
+
+// Case sensitive
+/*Case sensitive significa que o sistema diferencia letras maiúsculas de minúsculas. Ou seja, "Bruno" é diferente de "bruno".*/
+
+// Sintaxe básica e comentário
+/*Sintaxe básica é o conjunto de regras que definem como escrever corretamente os comandos de uma linguagem de programação*/
+console.log("ola mundo")
+
+// Declaração de variáveis(var, let, const)
+/*Na programação, variáveis são como "caixinhas" onde você guarda informações (números, textos, listas etc.) para usar depois.
+Declarar uma variável significa criar essa caixinha e dar um nome para ela.*/
+ /*Imagine que você tem caixinhas para guardar informações. var(Você pode abrir, fechar e trocar o conteúdo várias vezes.) let (Você também pode trocar o conteúdo.
+ const(Você não pode trocar o conteúdo depois de guardar) var (modelo antigo de escrever)*/
+
+ let nome = "bruno"
+ console.log(nome)
+ nome ="rafael"
+ console.log(nome)
+ const idade=19
+ console.log(idade)
+ 
+
+// Console e Debug
+/*console(O console é uma ferramenta usada para mostrar mensagens no navegador ou ambiente de desenvolvimento.
+Você usa o console.log() para ver o que está acontecendo no seu código.Outros tipos de console:console.log() → mostra mensagens comuns
+console.error() → mostra erros
+console.warn() → mostra avisos
+console.table() → mostra dados em formato de tabela
+Debug (ou "depuração") é o processo de encontrar e corrigir erros no código.
+Você pode usar o console.log() para ajudar, mas também pode usar ferramentas do navegador (como o DevTools do Chrome ou Edge).*/
+/*console.warn("atencao")
+console.info("informacao")
+console.erro("erro")*/
+
+
+//Tipos de dados
+// string
+let cidade = "americana"
+//number
+let salario = 2000
+//boolean
+let fumante = false
+
+console.log(typeof cidade)
+console.log(typeof 2000)
+console.log(typeof fumante)
+
+
+
+
+// Operadores
+//     Operador Atribuição
+
+//     Operadores aritméticos
+
+//     Operadores relacionais
+
+//     Operadores lógicos
+
+// Estrutura
+//     Estrutura de controle/decisão
+
+//     Laços de repetição
+
+// Arrays --> vetor
+
+// Funções personalizadas
