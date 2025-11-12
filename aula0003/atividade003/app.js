13eea95617596fbc3519b542d15786c9bd067f98
bhcabrera1991 <brunohenriquecabrera@gmail.com>
2025-11-12 11:13:14 -0300

    inclusao da funcao fnJogo

--- aula0003/atividade003/app.js
+++ aula0003/atividade003/app.js
@@ -1,11 +1,11 @@
 function fnJogo(){
-    let chute = 0, numeroMagico =3
-    chute=document.getElementById("valor").value 
-
+    let chute = 0, numeroMagico = Math.floor(Math.random()*10)
+    chute=document.getElementById("valor").value
     if(chute == numeroMagico){
         document.getElementById("resultado").innerText="Acertou"
     }else{
-        document.getElementById("resultado").innerText="errou"
+        document.getElementById("resultado")
+        alert("Errou " +numeroMagico)
     }
 }
 
